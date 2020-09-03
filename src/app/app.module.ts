be2b748be4945d8d93c155df54a39fcea002ce61
theooprea <opreatheodor54@gmail.com>
2020-09-03 13:31:51 +0300

    home and contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,8 +18,10 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
-import { AppRoutingModule } from './app-routing/app-routing.module';
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -43,7 +45,10 @@ import { DishService } from './services/dish.service';
     MatListModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [
+    DishService,
+    PromotionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
